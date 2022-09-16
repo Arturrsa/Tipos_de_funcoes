3453bf52876f88d5bfcbee8de13ce9f970469803
artur henrique <arturr.sanntos@gmail.com>
2022-09-16 11:03:08 -0300

    Create Ex.js

---
+++ Ex.js
@@ -0,0 +1,20 @@
+
+function simples(){
+
+    console.log("Está é uma função com sintaxe simples.")
+    }
+    simples()
+    
+    
+    
+    var soma = function (a,b) {
+              return a+b;
+    }
+    console.log(soma(5,8))
+    
+    
+   
+    
+    var soma = (a,b)=>a+b
+    console.log(soma(5,8))
+   
